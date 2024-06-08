6b6affd47ad92d67ce5e78b2c1df90312911b11e
Jacek <j.ciszewski@o2.pl>
2024-06-08 21:00:17 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,5 @@
+from main_window import MainWindow
+
+if __name__ == "__main__":
+    main_window = MainWindow()
+    main_window.mainloop()
---
+++ main_window.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+class MainWindow(tk.Tk):
+
+    def __init__(self):
+        super().__init__()
+        self.geometry("412x550")
+        self.title("Task List")
