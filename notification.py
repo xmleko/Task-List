14b8ce3cdcb1a5b5ef72348ff3bdd571c6a38e4e
Jacek <j.ciszewski@o2.pl>
2024-06-22 23:19:37 +0200

    delete

--- notification.py
+++
@@ -1,8 +0,0 @@
-from win10toast import ToastNotifier
-class Notification:
-    def __init__(self):
-        self.toast = ToastNotifier()
-
-    def Notify(self):
-        self.toast.show_toast("Notification", "Notification", duration=5)
-
